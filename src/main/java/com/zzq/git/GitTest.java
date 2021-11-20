1921f3305e206c07b9c74e38e62b23f9b7f92329
zhang <2955742336@qq.com>
2021-11-20 20:18:28 +0800

    first commit

---
+++ src/main/java/com/zzq/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zzq.git;
+
+/**
+ * @program: git
+ * @description:
+ * @author: ZHANG
+ * @create: 2021-11-20 20:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
